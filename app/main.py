5c2e8223b096c203219f34d31bf416e1f05049d4
João Ernane Barbosa <43070655+jovemadulto@users.noreply.github.com>
2025-08-04 10:47:36 -0300

    Documenta a aplicação
    Adiciona metadados simples na documentação principal da API.

--- app/main.py
+++ app/main.py
@@ -12,7 +12,28 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Analisador de Contratos com IA | Biofy",
+    description="""
+Esta aplicação facilita a análise de contratos jurídicos utilizando o auxílio de inteligência artificial generativa.
+
+Por meio desta interface o usuário pode fazer o upload de documentos nos formatos .pdf e .docx para serem analisados.
+
+São retornados detalhes críticos dos contratos como:
+- As partes envolvidas;
+- Valores negociados;
+- Obrigações de cada uma das partes;
+- Vigência do contrato;
+- Cláusulas de rescisão;
+""",
+    summary="Analisador de Contratos com IA Generativa.",
+    version="0.0.1",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
 app.include_router(contracts_router)
 
 
