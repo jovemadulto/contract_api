9c8794a50ed617bd74cb64a80dc5ebc4adddb335
João Ernane Barbosa <43070655+jovemadulto@users.noreply.github.com>
2025-08-04 11:28:20 -0300

    Organiza endpoints
    Organiza a página principal de documentação dos endpoints. Inclui tags e retira grandes blocos de textos de dentro de parâmetros da classe principal.

--- app/main.py
+++ app/main.py
@@ -12,9 +12,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title="Analisador de Contratos com IA | Biofy",
-    description="""
+application_description = """
 Esta aplicação facilita a análise de contratos jurídicos utilizando o auxílio de inteligência artificial generativa.
 
 Por meio desta interface o usuário pode fazer o upload de documentos nos formatos .pdf e .docx para serem analisados.
@@ -25,19 +23,29 @@ São retornados detalhes críticos dos contratos como:
 - Obrigações de cada uma das partes;
 - Vigência do contrato;
 - Cláusulas de rescisão;
-""",
+"""
+
+tags_metadata = [
+    {"name": "login", "description": "Interface para login na aplicação."},
+    {"name": "contracts", "description": "Operações de envio e retorno de contratos."},
+]
+
+app = FastAPI(
+    title="Analisador de Contratos com IA | Biofy",
+    description=application_description,
     summary="Analisador de Contratos com IA Generativa.",
     version="0.0.1",
     license_info={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
+    openapi_tags=tags_metadata,
 )
 
 app.include_router(contracts_router)
 
 
-@app.post("/login", response_model=Token)
+@app.post("/login", response_model=Token, tags=["login"])
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     if not authenticate_user(form_data.username, form_data.password):
         raise HTTPException(status_code=400, detail="Usuário ou senha inválidos")
